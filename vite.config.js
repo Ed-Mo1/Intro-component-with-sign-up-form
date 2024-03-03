4ec0de6896dd50c5309c564fec1bfc658ffe6238
eyad <eyadmuhammad90@gmail.com>
2024-03-04 00:00:17 +0200

    fixed bugs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/intro-component-with-signup-form-master/',
+  base: '/Intro-component-with-sign-up-form/',
   plugins: [react()],
 })
